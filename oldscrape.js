65af2be4265628b461f35eafc8344f007699be63
Harshal Thakare <harshalthakre.07@gmail.com>
2017-10-10 01:37:38 +0530

    n3 added

--- oldscrape.js
+++ oldscrape.js
@@ -1,19 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-/////////// First Block
-request('https://questale.com/', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-
-    $('.cat-v2-hom-list').find('a').each(function() {
-      var mainurl=$(this).attr('href');
-      console.log("main url is: "+mainurl);
-
-/////////////////////////Second block
-
-request(mainurl, function (error, response, html) {
-  var count=0;
+request('https://questale.com/report/category/aerospace-defense', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
     $('.page-blog').each(function(i, element){
@@ -23,8 +11,8 @@ request(mainurl, function (error, response, html) {
       var url=data.children().first().children().first().attr('href');
       title = data.children().first().text().trim();
       console.log(title);
-      console.log(count+++"secondary link is: "+url);
-      //console.log(url);
+      console.log("link is: ");
+      console.log(url);
 
 //////////////////////////
 request(url, function(error, response, html){
@@ -38,7 +26,7 @@ request(url, function(error, response, html){
       var data = $(this);
       title = data.children().first().next().text().trim();
     //  release = data.children().first().next().next().text().trim();//data.children().last().children().last().text().trim();
-   console.log("blog is: "+title)
+    console.log("blog is: "+title)
     })
   }
 })
@@ -46,16 +34,3 @@ request(url, function(error, response, html){
     });
   }
 });
-
-
-
-
-/////////////////////////end second block
-
-});
-
-  }
-});
-
-
-//////////end first block
